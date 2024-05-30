19d15ace42c4a63a880885c2f197671d3ef8ae98
Yersa Nordman <finwo@pm.me>
2024-05-30 21:04:37 +0200

    Nav highlighting

--- src/webgui/controller/partial.ts
+++ src/webgui/controller/partial.ts
@@ -1,3 +1,4 @@
+import { URL                                   } from 'url';
 import { Controller, Get, Req, Res, Middleware } from '@finwo/router';
 import { FastifyReply                          } from 'fastify';
 import { AuthenticatedRequest                  } from '@identity/model/authenticated-request';
@@ -16,14 +17,22 @@ export class PartialController {
     @Req() req: AuthenticatedRequest,
     @Res() res: FastifyReply
   ) {
+
     if (!req.auth) {
       res.statusCode = 204;
       res.header('HX-Redirect', '/ui/login');
       return res.send();
     }
 
+    // Parse & normalize referer
+    const referer = new URL(req.headers['hx-current-url'] || req.headers.referer || '');
+    if (referer.pathname.substring(referer.pathname.length-1) == '/') {
+      referer.pathname = referer.pathname.substring(0, referer.pathname.length - 1);
+    }
+
     res.header('Content-Type', 'text/html');
     res.send(this.template.render('partial/nav.html', {
+      referer,
       user: req.auth.user,
       site: {
         title: 'MyNVR',
