71f82629faac3e2bf7ba95bdba85321f175854a0
Yersa Nordman <finwo@pm.me>
2024-06-11 19:58:21 +0200

    Dedicated cameracontroller in webgui, it was getting messy

--- src/webgui/controller/index.ts
+++ src/webgui/controller/index.ts
@@ -1,4 +1,20 @@
+import { Controller, Get, Res } from '@finwo/router';
+import { FastifyReply } from 'fastify';
+
+@Controller()
+class IndexController {
+  @Get('/ui')
+  redirect(
+    @Res() res: FastifyReply
+  ) {
+    res.redirect('/ui/cameras/');
+  }
+}
+
 export const controllers = [
+  IndexController,
+  require('./camera').CameraController,
+
   require('./assets').AssetController,
   require('./authentication').AuthenticationController,
   require('./form').FormController,
--- src/webgui/controller/page.ts
+++ src/webgui/controller/page.ts
@@ -1,11 +1,6 @@
-import { Controller, Middleware, Get, Delete, Res, Req } from '@finwo/router';
-import { FastifyRequest, FastifyReply   } from 'fastify';
-import { AuthenticatedRequest           } from '@identity/model/authenticated-request';
-import   authenticated                    from '@identity/middleware/authenticated';
-import   requireAuthentication            from '@identity/middleware/require-authentication';
-
-import { Template } from '@webgui/template';
-import { CameraRepository } from '@nvr/repository/camera';
+import { Controller, Get, Res } from '@finwo/router';
+import { FastifyReply         } from 'fastify';
+import { Template             } from '@webgui/template';
 
 const commonData = {
   site: {
@@ -17,61 +12,7 @@ const commonData = {
 export class PageController {
   constructor(
     private template: Template,
-    private cameraRepository: CameraRepository,
   ) {}
-
-  @Get()
-  async camerasPage(
-    @Res() res: FastifyReply
-  ) {
-    res.header('Content-Type', 'text/html');
-    res.send(this.template.render('page/dashboard.html', {
-      ...commonData,
-    }));
-  }
-
-  @Get('/cameras/:name')
-  async cameraPage(
-    @Req() req: FastifyRequest,
-    @Res() res: FastifyReply
-  ) {
-    res.header('Content-Type', 'text/html');
-    res.send(this.template.render('page/camera.html', {
-      ...commonData,
-      camera: {
-        name: (req.params as Record<string, string>).name,
-      },
-      mediamtx: {
-        hls   : process.env.MEDIAMTX_HLS,
-        webrtc: process.env.MEDIAMTX_WEBRTC,
-      },
-    }));
-  }
-
-  @Delete('/cameras/:name')
-  @Middleware(authenticated)
-  @Middleware(requireAuthentication())
-  async deleteCamera(
-    @Req() req: AuthenticatedRequest,
-    @Res() res: FastifyReply
-  ) {
-    if (!req.auth) throw new Error();
-    const cameraName = (req.params as Record<string, string>).name;
-    if (!cameraName) throw new Error();
-
-    await this.cameraRepository.delete(cameraName);
-
-    const isHtmx = (req.headers['hx-request'] && req.headers['hx-request'] == 'true');
-    if (isHtmx) {
-      res.statusCode = 204;
-      res.header('HX-Redirect', '/ui/');
-      return res.send();
-    } else {
-      res.statusCode = 302;
-      res.header('Location', '/ui/');
-      return res.send();
-    }
-  }
 
   @Get('/users')
   async usersPage(
